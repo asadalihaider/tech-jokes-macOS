7818a18514c0dc92344c7032ce0c41f0de0e6cd5
Asad Ali Haider <108663729+asadalihaider@users.noreply.github.com>
2023-12-31 20:31:11 +0500

    Initial Commit

---
+++ Tech Jokes/Tech_JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tech_JokesApp.swift
+//  Tech Jokes
+//
+//  Created by Asad Ali Haider on 31/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tech_JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
