cd36fd28636d9fa7dd8901c1d0fc2587d3ae3458
Asad Ali Haider <108663729+asadalihaider@users.noreply.github.com>
2023-12-31 22:10:03 +0500

    create a basic layout for the joke view

--- Tech Jokes/JokeView.swift
+++ Tech Jokes/JokeView.swift
@@ -8,12 +8,41 @@
 import SwiftUI
 
 struct JokeView: View {
+    @State private var jokeString = "No Joke Available"
+    @State private var fetching = false
+    @AppStorage("jokeLanguage") var jokeLanguage: JokeLanguage = .english
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack(alignment: .center, content: {
+                // app logo
+                Image("TJ")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+
+                // add picker to select language
+                VStack {
+                    Text("Select Language")
+                        .font(.body)
+                    Picker("", selection: $jokeLanguage) {
+                        ForEach(JokeLanguage.allCases, id: \.self) { item in
+                            Text(item.rawValue).tag(item)
+                        }
+                    }
+                    .pickerStyle(MenuPickerStyle())
+                }
+            })
+            
+            // show loader while calling API
+            if fetching {
+                ProgressView()
+                    .padding(.top, 10.0)
+            } else {
+                // display joke
+                Text(jokeString)
+                    .minimumScaleFactor(0.5)
+                    .padding(.top, 10.0)
+            }
+            Spacer()
         }
         .padding()
     }
@@ -21,4 +50,5 @@ struct JokeView: View {
 
 #Preview {
     JokeView()
+        .frame(width: 255, height: 300)
 }
